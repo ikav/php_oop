fb5fe8c6c6116a0ee1aa758bc8e1600506ba2415
ikav <ikav@mail.ru>
2018-12-01 15:48:02 +0500

    Вывел все картинки через цикл for

--- 03/index.php
+++ 03/index.php
@@ -1,8 +1,17 @@
 <?php
 
+  $items = array (
+      'public/img/eagle-bald.jpg',
+      'public/img/eagle-eyes.jpg',
+      'public/img/owl.jpg',
+      'public/img/marsh-harrier.jpg',
+      'public/img/falcon.jpg'
+  );
+  
   // Подгружаем и активируем автозагрузчик Twig-а
   require_once 'Twig/Autoloader.php';
   Twig_Autoloader::register();
+  
   
   try {
     // Указывает, где хранятся шаблоны
@@ -13,10 +22,14 @@
     $template = $twig->loadTemplate('galary.tmpl');
     // Передаем в шаблон переменные и значения
     // Выводим сформированное содержание
+//    echo $template->render(array(
+//      birdPath => 'public/img/eagle-bald.jpg',
+//      birdName => 'Целеустремленный'
+//    ));
     echo $template->render(array(
-      birdPath => 'public/img/eagle-bald.jpg',
-      birdName => 'Целеустремленный'
+      'items' => $items
     ));
+    
   }
   catch (Exception $e) {
    die('ERROR: ' . $e->getMessage());
