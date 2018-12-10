643c0b2c3d791cf1c2990a97366ea543344b7906
ikav <ikav@mail.ru>
2018-12-10 22:09:43 +0500

    Исправлена ошибка, из-за которой не запускался фреймворк

--- 05/autoload.php
+++ 05/autoload.php
@@ -10,7 +10,7 @@ function gbStandardAutoload($className)
         'controller',
         'data/migrate',
         'lib',
-//        'lib/smarty',
+        'lib/smarty',
         'lib/commands',
         'model'
     ];
